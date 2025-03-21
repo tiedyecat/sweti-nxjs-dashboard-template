b2133e86a9bc8118aac4c13e2e8a54e0346e6923
tiedyecat <derek.jackson@swetiservices.com>
2025-03-20 23:41:14 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial, sans-serif" }}>
+      <h1>👋 Welcome to Sweti Dashboard</h1>
+      <p>This is your all-in-one paid media reporting dashboard.</p>
+    </div>
+  );
+}
 
