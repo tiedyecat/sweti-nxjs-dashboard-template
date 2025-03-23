6a9382f7edd2e386850fa166c0e74de4cc4505a2
tiedyecat <derek.jackson@swetiservices.com>
2025-03-23 12:09:56 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,5 @@
-import { useEffect } from "react";
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    document.documentElement.classList.add("dark");
-  }, []);
-
   return <Component {...pageProps} />;
 }
