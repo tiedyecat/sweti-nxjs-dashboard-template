241425b984f5ef1c1657e89af3e5e698fb933820
tiedyecat <derek.jackson@swetiservices.com>
2025-03-24 01:04:03 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      './pages/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}',
-    ],
-    darkMode: 'class', // or false, depending on your setup
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  darkMode: 'class', // Enables dark mode via a class
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
